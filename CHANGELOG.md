e585dafa878f4797c0d4d1a66e6e551a436d80c4
Alexandre Hulsken <alexandre.hulsken@interhaptics.com>
2022-12-01 11:30:10 +0100

    release(): expand mobile SDK

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,10 @@
+# 1st of December 2022
+# v1.0.2
+
++ Add slider scripts for the mobile SDK
+
+---
+
 # 29th of November 2022
 # v1.0.1
 
---
+++ Runtime/Platforms/Mobile/AHapticsSlider.cs
@@ -0,0 +1,58 @@
+﻿/* ​
+* Copyright © 2022 Go Touch VR SAS. All rights reserved. ​
+* ​
+*/
+
+using UnityEngine;
+using UnityEngine.UI;
+
+
+namespace Interhaptics.Platforms.Mobile
+{
+
+    /// <summary>
+    /// The abstract class to inherit to use the haptics with the Unity's slider.
+    /// </summary>
+    [RequireComponent(typeof(Slider))]
+    public abstract class AHapticsSlider : MonoBehaviour
+    {
+
+        #region Properties
+        public int MaterialIndex => materialIndex;
+        #endregion
+
+        #region Variables
+        /// <summary>
+        /// The index of the material.
+        /// </summary>
+        [SerializeField, Min(0)]
+        private int materialIndex = 0;
+
+        protected Slider _slider = null;
+        #endregion
+
+        #region Life Cycle
+        protected virtual void Awake()
+        {
+            _slider = gameObject.GetComponent<Slider>();
+            _slider.wholeNumbers = false;
+        }
+
+        protected virtual void OnEnable()
+        {
+            _slider.onValueChanged.AddListener(OnValueChanged);
+        }
+
+        protected virtual void OnDisable()
+        {
+            _slider.onValueChanged.RemoveListener(OnValueChanged);
+        }
+        #endregion
+
+        #region Abstracts
+        protected abstract void OnValueChanged(float value);
+        #endregion
+
+    }
+
+}
